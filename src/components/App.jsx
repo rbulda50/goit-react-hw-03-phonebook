57dfa5aacb32a184d152a8f6b6b194b6d2d1e5e6
rbulda50 <117113002+rbulda50@users.noreply.github.com>
2023-05-09 17:32:36 +0300

    updated
    added contact's objects

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,7 +10,11 @@ const LS_KEY = 'contacts';
 
 export class App extends Component {
   state = {
-    contacts: [],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },],
     filter: '',
   };
 
@@ -28,7 +32,7 @@ export class App extends Component {
     const { contacts } = this.state;
 
     if (prevContacts !== contacts) {
-      localStorage.setItem(LS_KEY, JSON.stringify(this.state.contacts))
+      localStorage.setItem(LS_KEY, JSON.stringify(contacts))
     };
   };
 
@@ -59,9 +63,9 @@ export class App extends Component {
   };
 
   getContacts = () => {
-    const { contacts } = this.state;
+    const { contacts, filter } = this.state;
 
-    const normalizedFilter = this.state.filter.toLowerCase();
+    const normalizedFilter = filter.toLowerCase();
     return contacts.filter((contact) =>
       contact.name.toLowerCase().includes(normalizedFilter)
     );
@@ -74,6 +78,8 @@ export class App extends Component {
   };
 
   render() {
+    const { filter } = this.state;
+
     const visibleContacts = this.getContacts();
     return (
       <div className={css.Container}>
@@ -83,7 +89,7 @@ export class App extends Component {
 
         <h2>Contacts</h2>
         <Filter
-          filter={this.state.filter}
+          filter={filter}
           changeFilter={this.changeFilter} />
         <ContactList
           visibleContacts={visibleContacts}
